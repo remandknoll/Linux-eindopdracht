1cafefff69fb01923e535777e75974eedf3a349e
Dennis Vrieling <de.vrieling@st.hanze.nl>
2017-06-27 20:09:40 +0200

    Salt scripts begin

---
+++ Salt-install-config-script-master.sh
@@ -0,0 +1,17 @@
+#root maken als nodig
+sudo -i
+
+#install salt-master + confirm alles
+echo "*********** INSTALLING SALT-MASTER BITCHES ***********"
+sudo apt-get install salt-master -y
+
+#install salt-minion + confirm alles
+echo "*********** INSTALLING SALT-MINION BITCHES ***********"
+sudo apt-get install salt-minion -y
+
+#CONFIG
+#Configure master file
+echo "*********** Effe config goed doen, ok. ***********"
+sed -i 's/#interface: 0.0.0.0/interface: 10.0.0.1/g' /etc/salt/master
+
+
---
+++ Salt-install-script.sh
@@ -0,0 +1,15 @@
+#root maken als nodig
+sudo -i
+
+#install salt-master + confirm alles
+echo "*********** INSTALLING SALT-MASTER BITCHES ***********"
+sudo apt-get install salt-master -y
+
+#install salt-minion + confirm alles
+echo "*********** INSTALLING SALT-MINION BITCHES ***********"
+sudo apt-get install salt-minion -y
+
+#CONFIG
+#Configure minion file
+echo "*********** Effe configs goed doen, ok. ***********"
+sed -i 's/#master: salt/master: 10.0.0.1/g' /etc/salt/minion
