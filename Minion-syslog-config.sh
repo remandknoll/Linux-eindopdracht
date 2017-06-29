2b01373d4846575337da0c230a9ba2357b715b3e
remandknoll <r.d.r.knol@st.hanze.nl>
2017-06-29 15:02:24 +0200

    Syslog salt minion config

---
+++ Minion-syslog-config.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+#COPYRIGHT REMAND KNOL - 339249
+echo "*.* @10.5.0.157:514" >> /etc/rsyslog.d/50-default.conf
+sed -i '#cron/cron/g' /etc/rsyslog.d/50-default.conf
+service rsyslog restart
