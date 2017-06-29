4ef2f78e77e9833c70497a0b0337206f63ab3a8c
remandknoll <r.d.r.knol@st.hanze.nl>
2017-06-29 15:08:16 +0200

    Update minion config

--- Minion-syslog-config.sh
+++ Minion-syslog-config.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 #COPYRIGHT REMAND KNOL - 339249
 echo "*.* @10.5.0.157:514" >> /etc/rsyslog.d/50-default.conf
-sed -i '#cron/cron/g' /etc/rsyslog.d/50-default.conf
+sed -i 's/#cron/cron/g' /etc/rsyslog.d/50-default.conf
 service rsyslog restart
